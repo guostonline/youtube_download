9caa1b288b047ae132238213c65421dec0adb19f
chakib elfil <guostonline@gmail.com>
2021-02-10 11:03:29 +0100

    second

--- youtube.py
+++ youtube.py
@@ -2,8 +2,9 @@ import bcolors
 from pytube import YouTube
 from termcolor import colored
 
-print(dir(bcolors)
+
 #url=input("Set youtube URL please : ")
+i=0
 url=input(colored('Entree Youtube link : ',"yellow"))
 youtube=YouTube(url)
 a=bcolors
@@ -12,19 +13,21 @@ video.filter(progressive=True)
 
 video.order_by("resolution")
 #print('\033[93m'+ video.get_highest_resolution())c
-i=0
+
 for stream in video:
     i+=1
     print(str(i)+colored(stream,"green") )
-choise=int(input(colored('Select wich video want to download : ',"yellow")))
+choise=input(colored('Select wich video want to download : ',"yellow"))
+if choise.isdigit:
+    choise=int(choise)
+    print(colored(video[choise-1],"blue"))
 
 
-print(colored(video[choise-1],"blue"))
 
 
 validation=input(colored("Entre yes : ","blue"))
 if validation=="yes":
     video[choise-1].download(output_path="C://Users//guost//Videos")
-    print ("is done")
+    print (colored("is done","yellow"))
 
        
